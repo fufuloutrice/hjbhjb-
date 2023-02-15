3ed8cb8b0f076228af36fdb226fc5b1d583519b3
fufuloutrice <maithilydqb@icloud.com>
2023-02-15 18:52:07 -0500

    fau quue ou

---
+++ annimaux'.py
@@ -0,0 +1,23 @@
+choice1 = 'animal'
+input()
+if choice1 == 'animal':
+    print('caniche', 'felin', 'volatile', 'reptile', 'rongeur')
+
+caniche = [f" 'labradro', 'Labrador retriever', 'labordro boxeur', 'chiba', 'dlamatiain', 'bouldog', "
+                    f" 'chien-loup', 'Golden Retriever', 'shih-tzu', "
+                    f" 'sharpeï', 'Husky', 'typeSpitz', 'teckel' "]
+
+felin = [f" 'abyssin',  'Highland Lynx', 'Chat de gouttière', 'Maine Coon',"
+      f"'Bleu russe', 'British shorthair', 'birman',  'sphynx',  'L’American Curl',' Ragdoll',"
+      f" ' European shorthair', 'norvégien' "]
+
+reptile = [" 'Dragon barbu d’Australie', 'Gecko léopard', 'Uromastyx', 'Lézard collier', 'anaconda', ' aspic', 'boa', 'cobra','couleuvre',  "]
+
+anaconda.
+aspic.
+boa.
+cobra.
+'couleuvre'.
+crotale.
+naja.
+python
---
+++ boutique.py
@@ -0,0 +1,7 @@
+boutique = input()
+def boutique():
+    if boutique == 'boutique':
+        print('nouriture, habita ou veteiment')
+        nouriture = input()
+        habita = input()
+        vetement = input(
